bf1e586cc55a951a8bdcbdd2f727c22891c2c7f9
Anderson Dias <andersondaraujo@gmail.com>
2009-12-12 02:21:45 -0300

    DRYing menu

--- _helpers.rb
+++ _helpers.rb
@@ -10,4 +10,23 @@ module Helpers
   def footer_class
     variable('footer_type') || 'internal_footer'
   end
+
+  def menu_list
+    [
+      [ 'apresentacao', 'Apresentação'],
+      [ 'comissao', 'Comissão Organizadora'],
+      [ 'mensagem', 'Mensagem&nbsp;do&nbsp;Presidente'],
+      [ '#', ' Programação'],
+      [ '#', ' Inscrições'],
+      [ '#', ' Trabalhos Científicos'],
+      [ '#', ' Local do Evento'],
+      [ '#', ' Agência de Turismo'],
+      [ '#', ' Conheça Natal'],
+      [ '#', ' Exposição'],
+      [ '#', ' Planta&nbsp;'],
+      [ '#', ' Expositores'],
+      [ '#', ' Patrocinadores'],
+      [ '#', ' Contato&nbsp;']
+    ]
+  end
 end
