6ab75fc181a0073f47e0cbdc1f2cf2f494744723
Paulo Fagiani <fagiani@imac.local.tink.com.br>
2009-12-12 21:16:19 -0300

    changing all pages to become posts and ordering them correctly

--- _helpers.rb
+++ _helpers.rb
@@ -10,19 +10,4 @@ module Helpers
   def layout_type
     variable('layout_type') || 'internal_page'
   end
-
-  def menu_list
-    [
-      [ 'apresentacao', 'Apresentação'],
-      [ 'comissao', 'Comissão Organizadora'],
-      [ 'inscricoes', ' Inscrições'],
-      [ 'trabalhos', ' Trabalhos Científicos'],
-      [ 'local', ' Local do Evento'],
-      [ 'agencia-de-turismo', ' Agência de Turismo'],
-      [ 'conheca-natal', ' Conheça Natal'],
-      [ 'exposicao', ' Exposição'],
-      [ 'parceiros', ' Parceiros'],
-      [ 'contato', ' Contato&nbsp;']
-    ]
-  end
 end
