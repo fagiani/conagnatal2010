90e68cb7b87590437838414b3f05ab2660b60909
Anderson dias <andersondaraujo@gmail.com>
2010-04-05 09:15:53 -0300

    Fix for countdown

--- js/counter.js
+++ js/counter.js
@@ -1,2 +1,2 @@
 var difference = (new Date("04/06/2010") - new Date()) / 1000;
-document.getElementById("countdown").innerHTML = Math.floor(( difference < 0 ? 0 : difference ) / (60 * 60 * 24)) + ' Dias';
+document.getElementById("countdown").innerHTML = Math.floor(( difference < 0 ? 0 : difference ) / (60 * 60 * 24)) + 1 + ' Dias';
